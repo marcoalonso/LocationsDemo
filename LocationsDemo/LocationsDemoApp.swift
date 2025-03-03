a8decd79f89cc588a14f51e197c378c916e9a916
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-03-03 09:27:52 -0600

    Initial Commit

---
+++ LocationsDemo/LocationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsDemoApp.swift
+//  LocationsDemo
+//
+//  Created by Marco Alonso Rodriguez on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
